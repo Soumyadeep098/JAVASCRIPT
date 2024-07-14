5f8116efbcdaa83cc34c2ea464b91d6b0eb32883
Soumyadeep098 <rickop444@outlook.com>
2024-07-14 13:06:29 +0530

    added

---
+++ 03_conversionoperations.js
@@ -0,0 +1,11 @@
+let score = "33abc";
+console.log(typeof score);
+
+score = Number(score) ;
+console.log(typeof score);
+console.log(score);
+
+let boolean = 1011111;
+boolean = Boolean(boolean);
+console.log(boolean);
+console.log(typeof boolean);
---
+++ let.const.var.js
@@ -0,0 +1,12 @@
+// variables
+
+const accountId = 1234589 ;
+let accountEmail = "random@xyz.com" ;
+var accountPassword = "12d4p9" ;
+accountCity = "Hyderabad"; // not  recommend
+console.log(accountCity) ;
+
+console.table([accountId , accountEmail . accountPassword , accountCity]);
+
+
+
